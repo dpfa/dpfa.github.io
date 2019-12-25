30836437da109c297b38239e0b792353537785f2
David Johnson-Bau <dgbau@me.com>
2019-12-25 11:01:59 -0700

    image hrerf

--- src/components/Artwork/Artwork.js
+++ src/components/Artwork/Artwork.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import './Artwork.scss'
+import React from "react";
+import "./Artwork.scss";
 
-function Artwork({data}) {
-  console.log(data)
-  let imgString = `/images/${data.image}`
+function Artwork({ data }) {
+  console.log(data);
+  let imgString = `/images/${data.image}`;
+  let url = `${process.env.PUBLIC_URL}/images/${data.image}`;
   return (
     <div className="col-6 artwork">
-      <img src={imgString}  alt={data.title} height="100" width="100"/>
+      <a href={url}>
+        <img src={imgString} alt={data.title} height="100" width="100" />
+      </a>
       <h6>{data.title}</h6>
-      <p>{data.medium} - ${data.price}</p>
+      <p>
+        {data.medium} - ${data.price}
+      </p>
     </div>
-  );  
+  );
 }
 
-export default Artwork
+export default Artwork;
