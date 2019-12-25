4debab18ea03379dce3c393566b0f73b22f05195
David Johnson-Bau <dgbau@me.com>
2019-12-24 20:50:38 -0700

    Updates

---
+++ precache-manifest.e5e5cefc32846c82dbfde54dac2df840.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34d535f5e7cafa3f86139f2d8cb58a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "404cc15250b099405c98",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3ff36ef70a2df103d7ad",
+    "url": "/static/css/main.cf24f507.chunk.css"
+  },
+  {
+    "revision": "404cc15250b099405c98",
+    "url": "/static/js/2.a949b638.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a949b638.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ff36ef70a2df103d7ad",
+    "url": "/static/js/main.2a63d0c7.chunk.js"
+  },
+  {
+    "revision": "561c35a88dfaa1cef740",
+    "url": "/static/js/runtime-main.3d08ebca.js"
+  }
+]);
